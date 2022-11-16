c2551efb9c7e3f53af0ee4c37aeffbf3acc9ff0f
André Bianchi <andrecarlosbb@gmail.com>
2022-11-15 22:47:58 -0300

    Create 2861.c

---
+++ C/2861.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    int i, t, letra[100];
+    
+    scanf("%d", &t);
+    
+    for(i = 0; i < t; i++){
+        
+        scanf("%s", &letra[i]);
+    
+        printf("gzuz\n");
+    }
+    
+    return 0;
+}
